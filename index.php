0efec30b7566bf11a1dd126e8e321a527ead2244
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-28 14:00:21 +0100

    Feat index.php - basic print of hotels-info

--- index.php
+++ index.php
@@ -40,4 +40,39 @@
 
     ];
 
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+
+<?php 
+ foreach ($hotels as $hotel):
+?>
+<div>
+<ul>
+<li> <?= $hotel["name"] ?> </li>
+<li> <?= $hotel["description"] ?> </li>
+<li> <?= $hotel["parking"] ?> </li>
+<li> <?= $hotel["vote"] ?> </li>
+<li> <?= $hotel["distance_to_center"] ?> </li>
+</ul>
+</div>
+
+<?php 
+ endforeach;
+?>
+
+</body>
+</html>
